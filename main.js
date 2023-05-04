6ace7cc21588f96f9b7a4c5b85175320e975af82
Aribo78 <89050884+Aribo78@users.noreply.github.com>
2023-05-04 19:28:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,5 +5,40 @@ function startClassification()
     modelReady);
 }
 function modelReady(){
-    classifer.classify(gotResults);
+    classifier.classify(gotResults);
+}
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+
+        document.getElementById("result_label").innerHTML = 'I can hear - '+
+        results[0].label;
+        document.getElementById("result_confidence").innerHTML = 'Accuracy - '+
+        (results[0].confidence*100).toFixed(2)+" %";
+        document.getElementById("result_label").style.color = "rgb("
+        +random_number_r+","+random_number_g+","+random_number_r+")";
+        document.getElementById("result_confidence").style.color = "rbg("
+        +random_number_r+","+random_number_g+","+random_number_r+")";
+
+        img = document.getElementById('ear1');
+
+        if (results[0].label == "DOGEcat") {
+            img.src = 'cat.png';
+
+        } else if (results[0].label == "DOGEdog") {
+            img.src = 'DOGE.png';
+
+        } else if (results[0].label == "DOGEdonkey") {
+            img.src = 'donkey.jpg';
+
+        }else{
+            img.src = 'dd5ed82b-b105-4b93-806f-1f9e718b56ec.png';
+
+        }
+    }
 }
